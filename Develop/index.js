b12281475633e90e62b02a4f03a13dc8229e52d5
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-27 18:00:56 -0500

    added require packages - fs and inquirer - started writing questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: 'Please enter the title for your project:',
+        name: 'title'
+    },
+    {
+        message: 'Enter a description for your project:',
+        name: 'description'
+    },
+    {
+        message: 'Please list any installation notes or instructions for your project:',
+        name: 'installation'
+    },
+    {
+        message: 'Please list any and all Usage information for your project',
+        name: 'usage'
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
